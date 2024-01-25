532bb39f90aec3d2491481e56203f26f44ea21b9
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-01-25 16:24:48 +0530

    lect-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import Home from './Home'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='two'   id='one'> helllo</h1>
+      <ul>
+        <li> hello</li>
+        <li> hello</li>
+        <li> hello</li>
+        <li> hello</li>
+        <li> hello</li>
+      </ul>
+      <Home/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    let data='home'
+  return (
+    
+    <div>ye kaun si file {data}
+                   
+    </div>
+   
+  )
+}
+
+export default Home
